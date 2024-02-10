f743be8966b304f5cde5a9f7800bebf9a6e73967
Khushwant Singh Rathore <31767649+khushwantSinghRathore@users.noreply.github.com>
2024-02-10 16:20:13 +0530

    Subscription Screen

--- src/app/screen/subscription/subscription.page.ts
+++ src/app/screen/subscription/subscription.page.ts
@@ -6,10 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./subscription.page.scss'],
 })
 export class SubscriptionPage implements OnInit {
+  options = [
+    { name: 'All' },
+    { name: 'Today' },
+    { name: 'Yeasterday' },
+    { name: 'This mounth' },
+    { name: 'continue watching' },
+  ];
 
-  constructor() { }
+  shorts = [
+    {
+      img: 'assets/Rectangle5.png',
+      des: 'The Beauty of Existence - Heart Touching Nasheed',
+      subd: '19,210,251 viewsJul • 1, 2016',
+    },
+    {
+      img: 'assets/Rectangle6.png',
+      des: 'The Beauty of Existence - Heart Touching Nasheed',
+      subd: '19,210,251 viewsJul • 1, 2016',
+    },
+    {
+      img: 'assets/Rectangle7.png',
+      des: 'The Beauty of Existence - Heart Touching Nasheed',
+      subd: '19,210,251 viewsJul • 1, 2016',
+    },
+    {
+      img: 'assets/Rectangle8.png',
+      des: 'The Beauty of Existence - Heart Touching Nasheed',
+      subd: '19,210,251 viewsJul • 1, 2016',
+    },
+  ];
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
