c12629ee5e568021585fa6b9ad31f98082f308cc
Khushwant Singh Rathore <31767649+khushwantSinghRathore@users.noreply.github.com>
2024-02-10 18:21:51 +0530

    library screen

--- src/app/screen/library/library.page.ts
+++ src/app/screen/library/library.page.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./library.page.scss'],
 })
 export class LibraryPage implements OnInit {
+  vids = [
+    {
+      img: 'assets/Rectangle5.png',
+      des: 'Heart Touching Nasheed #Shorts',
+      subd: 'An Naffe',
+    },
+    {
+      img: 'assets/Rectangle6.png',
+      des: 'Heart Touching Nasheed #Shorts',
+      subd: 'An Naffe',
+    },
+    {
+      img: 'assets/Rectangle7.png',
+      des: 'Heart Touching Nasheed #Shorts',
+      subd: 'An Naffe',
+    },
+    {
+      img: 'assets/Rectangle8.png',
+      des: 'Heart Touching Nasheed #Shorts',
+      subd: 'An Naffe',
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
