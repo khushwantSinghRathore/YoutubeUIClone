1bb0b1eb2f4e6ca50295f93f32c055f153a0cde1
Khushwant Singh Rathore <31767649+khushwantSinghRathore@users.noreply.github.com>
2024-02-10 20:02:52 +0530

    Download Screen

--- src/app/screen/download/download.page.ts
+++ src/app/screen/download/download.page.ts
@@ -6,10 +6,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./download.page.scss'],
 })
 export class DownloadPage implements OnInit {
+  vids = [
+    {
+      img: 'assets/Rectangle5.png',
+      des: 'The Beauty of Existence - Heart Touching Nasheed',
+      subd: '19,210,251 viewsJul • 1, 2016',
+    },
+    {
+      img: 'assets/Rectangle6.png',
+      des: 'The Beauty of Existence - Heart Touching Nasheed',
+      subd: '19,210,251 viewsJul • 1, 2016',
+    },
+    {
+      img: 'assets/Rectangle7.png',
+      des: 'The Beauty of Existence - Heart Touching Nasheed',
+      subd: '19,210,251 viewsJul • 1, 2016',
+    },
+    {
+      img: 'assets/Rectangle8.png',
+      des: 'The Beauty of Existence - Heart Touching Nasheed',
+      subd: '19,210,251 viewsJul • 1, 2016',
+    },
+    {
+      img: 'assets/Rectangle5.png',
+      des: 'The Beauty of Existence - Heart Touching Nasheed',
+      subd: '19,210,251 viewsJul • 1, 2016',
+    },
+    {
+      img: 'assets/Rectangle6.png',
+      des: 'The Beauty of Existence - Heart Touching Nasheed',
+      subd: '19,210,251 viewsJul • 1, 2016',
+    },
+    {
+      img: 'assets/Rectangle7.png',
+      des: 'The Beauty of Existence - Heart Touching Nasheed',
+      subd: '19,210,251 viewsJul • 1, 2016',
+    },
+    {
+      img: 'assets/Rectangle8.png',
+      des: 'The Beauty of Existence - Heart Touching Nasheed',
+      subd: '19,210,251 viewsJul • 1, 2016',
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
